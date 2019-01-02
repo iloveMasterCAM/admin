252a4146f375c663ab288ee6aff56cc6184b763e
iloveMasterCAM <1091060034@qq.com>
2019-01-02 16:00:54 +0800

    添加css

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/views/index'
+import user from '@/views/user'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: user
     }
   ]
 })
