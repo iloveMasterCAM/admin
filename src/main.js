7a5589ec758a3347813da9cc4e9aa460fdb007f1
Honest-man <487259468@qq.com>
2019-01-08 14:07:47 +0800

    honest

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import '../static/assets/scripts/klorofil-common'
 // 在 index.vue  里做  默认 上传营业执照  router 控制跳转
 // Vue.config.productionTip = false
+// honest honesthonesthonest 
 new Vue({
   el: '#wrapper',
   router,
